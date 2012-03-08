ed056829fee8940cb757537ec0bf8611bd93ea1c
P Purkayastha <ppurka@gmail.com>
2012-03-09 05:45:44 +0800

    Simplify the function as per Davide's suggestions

--- sagenb/data/sage/js/DoubleClickEvent.js
+++ sagenb/data/sage/js/DoubleClickEvent.js
@@ -13,35 +13,27 @@
  ***************************************************************************
  */
 
-MathJax.Callback.Queue(
-  MathJax.Hub.Register.StartupHook("MathEvents Ready", function () {
-    var HUB = MathJax.Hub;
-    //
-    // Redefine DblClick only if it is not a mobile device. For a mobile
-    // device MathJax-2.0 has double tap and hold for ContextMenu.
-    //
-    if (!HUB.Browser.isMobile) {
-      (function () {
-        var EVENT = MathJax.Extension.MathEvents.Event;
-        EVENT.DblClick = function(event){
-          return EVENT.Handler(event,"ContextMenu",this);
-        }
-      })();
-      //
-      // Disable the DblClick option from the menu. This is taken mostly
-      // from MathMenu.js
-      //
-      (function () {
-        var MENU = MathJax.Menu;
-        var MENUSETTINGS = HUB.config.menuSettings;
-        var trigger = MENU.menu.Find("Math Settings", "Zoom Trigger").menu;
-        trigger.items[2].disabled = true;
-        if (MENUSETTINGS.zoom === "Double-Click") {MENUSETTINGS.zoom = "None"}
-      })();
+MathJax.Hub.Register.StartupHook("MathEvents Ready", function () {
+  var HUB = MathJax.Hub;
+  //
+  // Redefine DblClick only if it is not a mobile device. For a mobile
+  // device MathJax-2.0 has double tap and hold for ContextMenu.
+  //
+  if (!HUB.Browser.isMobile) {
+    var EVENT = MathJax.Extension.MathEvents.Event;
+    EVENT.DblClick = function(event){
+      return EVENT.Handler(event,"ContextMenu",this);
     }
+    //
+    // Disable the DblClick option from the menu. This is taken mostly
+    // from MathMenu.js
+    //
+    var MENU = MathJax.Menu, MENUSETTINGS = HUB.config.menuSettings;
+    MENU.menu.Find("Math Settings", "Zoom Trigger","Double-Click").disabled = true;
+    if (MENUSETTINGS.zoom === "Double-Click") {MENUSETTINGS.zoom = "None"}
+  }
 
-    HUB.Startup.signal.Post("DoubleClickEvent Ready");
-  })
-);
+  HUB.Startup.signal.Post("DoubleClickEvent Ready");
+});
 
 MathJax.Ajax.loadComplete("[MathJax]/../sage/js/DoubleClickEvent.js");
